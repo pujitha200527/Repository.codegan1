fe8f1170007f43eb4473ebd5b84582308d47894d
pujitha200527 <pujithapalli3@gmail.com>
2026-04-15 15:54:04 +0530

    I started my day-1 class

---
+++ day1.py
@@ -0,0 +1,11 @@
+'''print("This is my first day at codegnan")
+What is python?
+-->Python is a high-level language means no need to worry about low-level management like memory allocation.
+-->python never save anything permanently,it will only save at run time
+-->python supports OOP'S
+-->This is also called dynamically-typed language
+'''
+a = 9
+b = "Python"
+print(type(a))
+print(type(b))
