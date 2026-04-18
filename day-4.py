b2ca52639e8dfec9c8fe9db8a6da4f77a0746a4a
pujitha200527 <pujithapalli3@gmail.com>
2026-04-18 10:55:58 +0530

    Today,I learned about string and string methods

---
+++ day-4.py
@@ -0,0 +1,20 @@
+'''Datatypes
+there are 2 types
+1.mutuable data type
+2.immutable data type
+mutable data type
+---------------------
+--> I can modify directly on the variable , which the data type have taken
+eg
+-- list,dict
+immutable
+---------------
+-->where can not be modified directly on the variable assign to the data type
+eg
+-- int,string
+
+
+any = " python is a programming language"
+print(any[10])'''
+num=int(input())
+print(f"{num}is a even")
