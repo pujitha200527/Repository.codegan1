69176d4838c3f61cc38b110004249d15ef990219
pujitha200527 <pujithapalli3@gmail.com>
2026-04-28 10:33:09 +0530

    today I learned about tuple and dictionary methods

---
+++ day-6.py
@@ -0,0 +1,41 @@
+'''Tuple
+-----
+-->tuple is a collection of different data types that represent by()and the items in the tuple seperated by comma..
+so = (1,"python",[7,8],(5,0),1)
+print(so)
+print(so[1][2])
+so = (1,"python",[7,8],(5,0),1)
+any = (5,6,7)
+print(so + any)
+Dictionary
+--------------
+-->Dict is a collection of key:value pair, where keys are immutable(string, int and tuple) and value are any data typethis is represented by {}
+methods
+--------------
+keys()
+------------
+-->This method is used to access only keys in the dictionary
+syntax-->dict.keys()
+eg:
+my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
+print(my.keys())
+values()
+------
+-->This is used to access only values in the dictionary
+syntax-->dict.values()
+eg:
+my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
+print(my.values())
+items()
+------------
+-->This method is used to access key:value pair in the dictionary
+syntax-->dict.items()
+eg:
+my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
+print(my.items())
+
+clear()
+
+'''
+my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
+print(my.items())
