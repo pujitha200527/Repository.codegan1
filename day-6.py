8d03a98b857f03773a3c834cafe676e67e020960
pujitha200527 <pujithapalli3@gmail.com>
2026-04-29 10:36:17 +0530

    Today i learned about set methods and type conversions methods

--- day-6.py
+++ day-6.py
@@ -1,41 +1,57 @@
-'''Tuple
------
--->tuple is a collection of different data types that represent by()and the items in the tuple seperated by comma..
-so = (1,"python",[7,8],(5,0),1)
-print(so)
-print(so[1][2])
-so = (1,"python",[7,8],(5,0),1)
-any = (5,6,7)
-print(so + any)
-Dictionary
---------------
--->Dict is a collection of key:value pair, where keys are immutable(string, int and tuple) and value are any data typethis is represented by {}
-methods
---------------
-keys()
-------------
--->This method is used to access only keys in the dictionary
-syntax-->dict.keys()
-eg:
-my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
-print(my.keys())
-values()
-------
--->This is used to access only values in the dictionary
-syntax-->dict.values()
-eg:
-my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
-print(my.values())
-items()
-------------
--->This method is used to access key:value pair in the dictionary
-syntax-->dict.items()
-eg:
-my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
-print(my.items())
-
-clear()
-
 '''
-my = {"Name":"Pujitha","age":"21","edu":"B.Tech"}
+Tuple
+-----
+-->tuple is a collection of different data types that represent by() and the items in the tuple is separeted by  comma..
+eg:
+so = (1,"python",[7,8],(5,0),1)
+print(so[1])
+
+so=(1,3,4,5)
+any=(2,6,7)
+print(so+any)
+Dictionary
+----------
+--->Dict is a collection of key : value pair, where keys are immutable such as (strings, integer, and tuple) and value are any data type this is represented by{}
+eg:
+my={"Name": "Puji",1 : 3,(1,2): 6,"Name" : "palli"}
+print(my)
+Methods
+-------
+1.key()
+-------
+-->This is method is used to access only keys in the dictionary
+syntax->dict.keys()
+eg:
+my={"Name": "puji",1 : 3,(1,2): 6}
+print(my.keys())
+2.value()
+--------
+--->this is used to acces only values in the dictionary
+syntax->dict.values()
+eg:
+my={"Name": "puji",1 : 3,(1,2): 6}
+print(my.values())
+3.items()
+---------
+--->This method is used to access key :value pair in the dictionary
+syntax-> dict.items()
+eg:
+my={"Name": "puji",1 : 3,(1,2): 6}
 print(my.items())
+4.clear()
+---------
+--->This clear() method is used to delete all the items in the dictionary
+syntax->dict.clear()
+eg:
+my={"name":"puji","age":21, "edu":"b.tech"}
+my.clear()
+print(my)
+5. update()
+----------
+--->this method is used to add new item(key:value)into the dictionary
+syntax->dict.update({key:value})
+eg:
+my={"name":"puji","age":21,"edu":"b.tech"}
+my.update({"role":"developer"})
+print(my)
+'''
